4fb244ce60bceb5393165c7ee13b072654168caf
NikosVarel <nikosvar93@gmail.com>
2021-09-10 13:46:06 +0300

    UPDATE

--- ProjectA/test_main.py
+++ ProjectA/test_main.py
@@ -8,6 +8,6 @@ class TestUserInfo:
         print(f"I {user[0]} with last name {user[1]} love to {user[3]} {quantity}!")
 
     def test_age(self,user):
-        print(f"{user[0]} {user[1]} is {user[2]} years old!")
+        print(f"{user[0]} {user[1]} is {int(user[2])} years old!")
 
 # pytest --html=Sheets\report.html -v
