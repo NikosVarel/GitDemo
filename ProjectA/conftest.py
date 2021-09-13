731d07ba7127647e18d700407e213e1d3cb12a3f
NikosVarel <nikosvar93@gmail.com>
2021-09-13 13:31:22 +0300

    UPDATE

--- ProjectA/conftest.py
+++ ProjectA/conftest.py
@@ -4,15 +4,19 @@ import openpyxl
 book = openpyxl.load_workbook("C:/Users/nikos/OneDrive/Υπολογιστής/BootCamps/GitDemo/ProjectA/Sheets/UserSheet.xlsx")
 sheet = book.active
 
-def ListOfUsers():
+def ListOfUsers(ind=None):
     sheet_list = []
     for i in range(2, sheet.max_row + 1):
-        user_list = []
-        for j in range(2, sheet.max_column + 1):
-            user_list.append(sheet.cell(row=i, column=j).value)
-            if len(user_list) == sheet.max_column - 1:
-                sheet_list.append(user_list)
-    return sheet_list
+        user_dict = {}
+        for j in range(2,sheet.max_column + 1):
+            user_dict[sheet.cell(row=1, column=j).value]=sheet.cell(row=i, column=j).value
+        sheet_list.append(user_dict)
+    if ind != None:
+        return sheet_list[ind]
+    else:
+        return sheet_list
+
+print(ListOfUsers())
 
 @pytest.fixture(params=ListOfUsers())
 def user(request):
--- ProjectA/test_main.py
+++ ProjectA/test_main.py
@@ -5,9 +5,9 @@ import pytest
 class TestUserInfo:
 
     def test_hobbie(self,user,quantity):
-        print(f"I {user[0]} with last name {user[1]} love to {user[3]} a {quantity}!")
+        print(f"I {user['first_name']} with last name {user['last_name']} love to {user['hobby']} a {quantity}!")
 
     def test_age(self,user):
-        print(f"{user[0]} {user[1]} is {int(user[2])} years old!")
+        print(f"{user['first_name']} {user['last_name']} is {int(user['age'])} years old!")
 
 # pytest --html=Sheets\report.html -v
